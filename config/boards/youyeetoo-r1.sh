5d172fca65bce0e06b23bbf695031531b5d17cce
noneNPC <123532486+noneNPC@users.noreply.github.com>
2026-09-12 17:40:36 +0800

    Update youyeetoo-r1.sh
    更换可用的add-apt-repository

--- config/boards/youyeetoo-r1.sh
+++ config/boards/youyeetoo-r1.sh
@@ -15,7 +15,7 @@ function config_image_hook__youyeetoo-r1() {
 
     if [ "${suite}" == "jammy" ] || [ "${suite}" == "noble" ]; then
         # Install panfork
-        chroot "${rootfs}" add-apt-repository -y ppa:jjriek/panfork-mesa
+        chroot "${rootfs}" add-apt-repository -y ppa:liujianfeng1994/panfork-mesa
         chroot "${rootfs}" apt-get update
         chroot "${rootfs}" apt-get -y install mali-g610-firmware
         chroot "${rootfs}" apt-get -y dist-upgrade
@@ -24,7 +24,7 @@ function config_image_hook__youyeetoo-r1() {
         chroot "${rootfs}" apt-get -y install libmali-g610-x11
 
         # Install the rockchip camera engine
-        chroot "${rootfs}" apt-get -y install camera-engine-rkaiq-rk3588
+        # chroot "${rootfs}" apt-get -y install camera-engine-rkaiq-rk3588
     fi
 
     return 0
