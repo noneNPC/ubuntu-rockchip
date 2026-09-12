47f9626854a1da274b181085e8a9fbae293db4e3
noneNPC <1472823422@qq.com>
2026-09-12 23:45:32 +0800

    Update youyeetoo-r1.sh

--- config/boards/youyeetoo-r1.sh
+++ config/boards/youyeetoo-r1.sh
@@ -15,7 +15,7 @@ function config_image_hook__youyeetoo-r1() {
 
     if [ "${suite}" == "jammy" ] || [ "${suite}" == "noble" ]; then
         # Install panfork
-        chroot "${rootfs}" add-apt-repository -y ppa:liujianfeng1994/panfork-mesa
+        chroot "${rootfs}" add-apt-repository -y ppa:jjriek/panfork-mesa
         chroot "${rootfs}" apt-get update
         chroot "${rootfs}" apt-get -y install mali-g610-firmware
         chroot "${rootfs}" apt-get -y dist-upgrade
