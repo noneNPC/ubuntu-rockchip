4ba87351265125fa1ee22bf756e047334e06cc11
noneNPC <1472823422@qq.com>
2026-09-13 20:07:56 +0800

    Update jammy.sh

--- config/suites/jammy.sh
+++ config/suites/jammy.sh
@@ -4,7 +4,7 @@ export RELASE_NAME="Ubuntu 22.04 LTS (Jammy Jellyfish)"
 export RELASE_VERSION="22.04"
 
 export KERNEL_REPO="https://github.com/SuperKali/linux-rockchip.git"
-export KERNEL_BRANCH="jammy"
+export KERNEL_BRANCH="noble"
 export KERNEL_FLAVOR="rockchip"
 
 export EXTRA_PPAS="jjriek/rockchip jjriek/rockchip-multimedia"
